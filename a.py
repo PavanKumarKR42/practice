62c08d804fc5d8c23118eb87e58b37865574cc5c
Pavan <pavanumar42@gmail.com>
2025-03-14 07:03:56 +0530

    added

---
+++ a.py
@@ -0,0 +1,135 @@
+
+i1948@Pavan MINGW64 ~/Desktop/githubpractice (master)
+$ git status
+warning: could not open directory 'Application Data/': Permission denied
+warning: could not open directory 'Cookies/': Permission denied
+warning: could not open directory 'Local Settings/': Permission denied
+warning: could not open directory 'My Documents/': Permission denied
+warning: could not open directory 'NetHood/': Permission denied
+warning: could not open directory 'PrintHood/': Permission denied
+warning: could not open directory 'Recent/': Permission denied
+warning: could not open directory 'SendTo/': Permission denied
+warning: could not open directory 'Start Menu/': Permission denied
+warning: could not open directory 'Templates/': Permission denied
+On branch master
+
+No commits yet
+
+Changes to be committed:
+  (use "git rm --cached <file>..." to unstage)
+        new file:   ../devops - theory/dev1.txt
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        ../../.VirtualBox/
+        ../../.anaconda/
+        ../../.bash_history
+        ../../.cache/
+        ../../.conda/
+        ../../.condarc
+        ../../.config/
+        ../../.continuum/
+        ../../.docker/
+        ../../.eclipse/
+        ../../.gitconfig
+        ../../.idlerc/
+        ../../.ipynb_checkpoints/
+        ../../.ipython/
+        ../../.jupyter/
+        ../../.lesshst
+        ../../.matplotlib/
+        ../../.node_repl_history
+        ../../.nuget/
+        ../../.p2/
+        ../../.packettracer
+        ../../.ssh/
+        ../../.th-client/
+        ../../.viminfo
+        ../../.vscode/
+        ../../AppData/
+        ../../Cisco Packet Tracer 7.3.0/
+        ../../Contacts/
+        ../../Data.ipynb
+        ../../De/
+        ../ATCFINAL.jff
+        ../Bug/
+        ../Cisco Packet Tracer.lnk
+        ../Engineering/
+        ../Google Services/
+        ../O T P Y R C/
+        ../Polygon zkEVM Bridge.lnk
+        ../Sonic Visualiser.lnk
+        ../cn pro/
+        ../cs/
+        ../desktop.ini
+        ../developingtools/
+        ./
+        ../gitsample/
+        ../hjsd.jpg
+        ../img1.png
+        ../img2.png
+        ../m/
+        ../pwc/
+        ../uTorrent Web.lnk
+        ../ubuntu-22.04.3-desktop-amd64.iso
+        ../voting-system/
+        ../../Documents/
+        ../../Downloads/
+        ../../Favorites/
+        ../../IdeaProjects/
+        ../../Links/
+        ../../Microsoft/
+        ../../Music/
+        ../../NTUSER.DAT
+        ../../NTUSER.DAT{2fb38bd0-e57b-11ef-b6a7-a24dc4a4035b}.TM.blf
+        ../../NTUSER.DAT{2fb38bd0-e57b-11ef-b6a7-a24dc4a4035b}.TMContain00000000000001.regtrans-ms
+        ../../NTUSER.DAT{2fb38bd0-e57b-11ef-b6a7-a24dc4a4035b}.TMContain00000000000002.regtrans-ms
+        ../../OneDrive/
+        ../../Pictures/
+        ../../README.md
+        ../../SIH-Bail-Reckoner/
+        ../../Saved Games/
+        ../../Searches/
+        ../../Tokenization.ipynb
+        ../../Untitled.ipynb
+        ../../Untitled1.ipynb
+        ../../Untitled10.ipynb
+        ../../Untitled11.ipynb
+        ../../Untitled12.ipynb
+        ../../Untitled13.ipynb
+        ../../Untitled14.ipynb
+        ../../Untitled15.ipynb
+        ../../Untitled16.ipynb
+        ../../Untitled17.ipynb
+        ../../Untitled18.ipynb
+        ../../Untitled19.ipynb
+        ../../Untitled2.ipynb
+        ../../Untitled20.ipynb
+        ../../Untitled21.ipynb
+        ../../Untitled22.ipynb
+        ../../Untitled3.ipynb
+        ../../Untitled4.ipynb
+        ../../Untitled5.ipynb
+        ../../Untitled6.ipynb
+        ../../Untitled7.ipynb
+        ../../Untitled8.ipynb
+        ../../Untitled9.ipynb
+        ../../Videos/
+        ../../WhatsApp Image 2024-08-29 at 12.43.35_8627befb.jpg
+        ../../a.exe
+        ../../anaconda3/
+        ../../anaemia child.ipynb
+        ../../battery-report.html
+        ../../eclipse-workspace/
+        ../../eclipse/
+        ../../gittur/
+        ../../hello/
+        ../../helloworld.c
+        ../../helloworld.exe
+        ../../ntuser.dat.LOG1
+        ../../ntuser.dat.LOG2
+        ../../ntuser.ini
+        ../../python
+        ../../source/
+        ../../test/
+        ../../w.txt
